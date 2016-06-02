932d569d44cf92153f7d285045d87980185d8318
ngockha96 <ngockha96@gmail.com>
2016-06-02 11:23:56 +0700

    Part 1

---
+++ part1_1_2/Project.cpp
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+
+using namespace std;
+	
+int main (void)
+	{
+		string u_foldername,u_filename;
+		cout << "Which folder do want to find ?";
+		getline (cin,u_foldername);
+		
+		cout << "Type in the name of the file.";
+		getline (cin,u_filename);
+	}
